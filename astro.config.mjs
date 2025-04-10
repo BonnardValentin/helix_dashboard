395585269bf4f84c60609c6f518dc015d01bb72f
Bonnard Valentin <val@2501.ai>
2025-04-10 13:05:45 +0200

    try to fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,9 @@ export default defineConfig({
   output: 'server',
   adapter: vercel({
     analytics: true,
-    nodeVersion: 'v18',
+    includeFiles: ['./public/**/*'],
+    functionPerRoute: false,
+    maxDuration: 60,
+    devCommand: 'npm run dev'
   }),
 }); 
